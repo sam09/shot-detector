d268b8a4c27d514852c7153490d04e2833c3c294
Sam Radhakrishnan <sk09idm@gmail.com>
2020-05-27 02:19:56 +0530

    Add option to use GPU.

--- scripts/scene_change_detector.sh
+++ scripts/scene_change_detector.sh
@@ -2,9 +2,29 @@
 my_fpath=$(readlink -f "$0")
 my_dpath=$(dirname "$my_fpath")
 
-#echo "my directory: $my_dpath"
+print_help_and_exit() {
+	echo "Usage instructions";
+	echo "scene_change_detector.sh <input_video> <output_file> [--use_cuda]";
+	exit 1;
+}
+
 input=$1
 output=$2
+if [[ -z "$input" ]]; then
+	echo "No input file found"
+	print_help_and_exit
+elif [[ -z "$output" ]]; then
+	echo "No output file found"
+	print_help_and_exit
+elif [[ -z "$3" ]]; then
+	use_cuda=0
+elif [[ "$3" -eq "--use_cuda" ]]; then
+	use_cuda=1
+else
+	echo "$3 Invalid option"
+	print_help_and_exit
+fi
+
 dur=$(mediainfo --inform="General;%Duration%" "$input")
 
 $my_dpath/scene_detect $input $output $dur $use_cuda
--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,22 @@
 
 int main (int argc, char* argv[])
 {
-	if (argc < 4) {
+	if (argc < 5) {
 		std::cout<<"Wrong number of arguments"<<std::endl;
 		return 1;
 	}
 	std::string input = std::string(argv[1]);
 	std::string output = std::string(argv[2]);
 	int duration = atoi(argv[3]);
-
+	int use_cuda = atoi(argv[4]);
+	
 	// Default values for video.
 	int threshold = 30;
 	int min_scene_len = 15;
 	int num_pixels_per_frame = 0;
 
 	ContentDetector *c = new ContentDetector(threshold, min_scene_len, num_pixels_per_frame);
-    SceneDetect s (false, input, output, duration, c);
+    SceneDetect s (use_cuda == 1, input, output, duration, c);
     std::cout<<"Starting video cut detection"<<std::endl;
     s.detect_cuts();
 }
